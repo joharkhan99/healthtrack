6f32f60f5c79d5138afbb0815048aaf499f6668e
single-byte <joharkhan1999@gmail.com>
2024-03-15 23:21:52 +0500

    Refactor pedometer_report.dart

--- lib/src/screens/pedometer_report.dart
+++ lib/src/screens/pedometer_report.dart
@@ -1,56 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:charts_flutter/flutter.dart' as charts;
-
-class BarChartData {
-  final String label;
-  final int value;
-
-  BarChartData(this.label, this.value);
-}
-
-List<charts.Series<BarChartData, String>> _createSeries(List<BarChartData> data) {
-  return [
-    charts.Series<BarChartData, String>(
-      id: 'Data',
-      data: data,
-      domainFn: (BarChartData datum, _) => datum.label,
-      measureFn: (BarChartData datum, _) => datum.value,
-      labelAccessorFn: (BarChartData datum, _) => '${datum.value}',
-      fillColorFn: (_, __) => charts.ColorUtil.fromDartColor(Colors.green).lighter,
-      // labelAccessorFn: (BarChartData datum, _) => '\$${datum.toString()}'
-      insideLabelStyleAccessorFn: (datum, index) => charts.TextStyleSpec(
-        color: charts.MaterialPalette.white,
-        fontSize: 12,
-      ),
-    )
-  ];
-}
-
-class VerticalBarChart extends StatelessWidget {
-  final List<BarChartData> data;
-
-  const VerticalBarChart(this.data, {super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: charts.BarChart(
-        _createSeries(data),
-        vertical: true,
-        // primaryMeasureAxis: null,
-        barRendererDecorator: charts.BarLabelDecorator<String>(),
-        defaultRenderer: charts.BarRendererConfig(
-          // cornerStrategy: const charts.ConstCornerStrategy(5),
-          groupingType: charts.BarGroupingType.grouped,
-          barGroupInnerPaddingPx: 0,
-          strokeWidthPx: 0,
-        ),
-        barGroupingType: charts.BarGroupingType.grouped,
-        primaryMeasureAxis: new charts.NumericAxisSpec(renderSpec: new charts.NoneRenderSpec()),
-      ),
-    );
-  }
-}
+import 'package:healthtrack/src/widgets/charts/pedometer_bar_chart.dart';
 
 class PedometerReport extends StatelessWidget {
   const PedometerReport({super.key});
@@ -68,14 +17,11 @@ class PedometerReport extends StatelessWidget {
     ];
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Pedometer Report'),
-      ),
-      body: Center(
-        child: Container(
-          height: 300,
-          padding: EdgeInsets.all(0),
-          child: VerticalBarChart(chartData),
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: SizedBox(
+          height: MediaQuery.of(context).size.height * 0.4,
+          child: PedometerBarChart(chartData),
         ),
       ),
     );
---
+++ lib/src/widgets/charts/pedometer_bar_chart.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:charts_flutter/flutter.dart' as charts;
+
+class BarChartData {
+  final String label;
+  final int value;
+
+  BarChartData(this.label, this.value);
+}
+
+List<charts.Series<BarChartData, String>> _createSeries(List<BarChartData> data) {
+  return [
+    charts.Series<BarChartData, String>(
+      id: 'Data',
+      data: data,
+      domainFn: (BarChartData datum, _) => datum.label,
+      measureFn: (BarChartData datum, _) => datum.value,
+      labelAccessorFn: (BarChartData datum, _) => '${datum.value}',
+      fillColorFn: (_, __) => charts.ColorUtil.fromDartColor(Colors.green).lighter,
+      insideLabelStyleAccessorFn: (datum, index) => const charts.TextStyleSpec(
+        color: charts.MaterialPalette.white,
+        fontSize: 10,
+      ),
+    )
+  ];
+}
+
+class PedometerBarChart extends StatelessWidget {
+  final List<BarChartData> data;
+  const PedometerBarChart(this.data, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return charts.BarChart(
+      _createSeries(data),
+      vertical: true,
+      barRendererDecorator: charts.BarLabelDecorator<String>(),
+      primaryMeasureAxis: const charts.NumericAxisSpec(renderSpec: charts.NoneRenderSpec()),
+      domainAxis: charts.OrdinalAxisSpec(
+        renderSpec: charts.SmallTickRendererSpec(
+          labelStyle: charts.TextStyleSpec(
+            fontSize: 14,
+            fontWeight: FontWeight.w700.toString(),
+            color: charts.ColorUtil.fromDartColor(Colors.grey.shade400), // Set label color to green
+          ),
+        ),
+      ),
+      defaultRenderer: charts.BarRendererConfig(
+        barRendererDecorator: charts.BarLabelDecorator<String>(),
+        cornerStrategy: const charts.ConstCornerStrategy(3), // Remove bar corner radius
+      ),
+    );
+  }
+}
