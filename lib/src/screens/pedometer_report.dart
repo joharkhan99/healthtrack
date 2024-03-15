89c3521200e2ef5e08a97a5f2a6ae800e86c03d4
single-byte <joharkhan1999@gmail.com>
2024-03-15 20:00:42 +0500

    Add vertical bar chart to PedometerReport screen

--- lib/src/screens/pedometer_report.dart
+++ lib/src/screens/pedometer_report.dart
@@ -1,16 +1,82 @@
 import 'package:flutter/material.dart';
+import 'package:charts_flutter/flutter.dart' as charts;
+
+class BarChartData {
+  final String label;
+  final int value;
+
+  BarChartData(this.label, this.value);
+}
+
+List<charts.Series<BarChartData, String>> _createSeries(List<BarChartData> data) {
+  return [
+    charts.Series<BarChartData, String>(
+      id: 'Data',
+      data: data,
+      domainFn: (BarChartData datum, _) => datum.label,
+      measureFn: (BarChartData datum, _) => datum.value,
+      labelAccessorFn: (BarChartData datum, _) => '${datum.value}',
+      fillColorFn: (_, __) => charts.ColorUtil.fromDartColor(Colors.green).lighter,
+      // labelAccessorFn: (BarChartData datum, _) => '\$${datum.toString()}'
+      insideLabelStyleAccessorFn: (datum, index) => charts.TextStyleSpec(
+        color: charts.MaterialPalette.white,
+        fontSize: 12,
+      ),
+    )
+  ];
+}
+
+class VerticalBarChart extends StatelessWidget {
+  final List<BarChartData> data;
+
+  const VerticalBarChart(this.data, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: charts.BarChart(
+        _createSeries(data),
+        vertical: true,
+        // primaryMeasureAxis: null,
+        barRendererDecorator: charts.BarLabelDecorator<String>(),
+        defaultRenderer: charts.BarRendererConfig(
+          // cornerStrategy: const charts.ConstCornerStrategy(5),
+          groupingType: charts.BarGroupingType.grouped,
+          barGroupInnerPaddingPx: 0,
+          strokeWidthPx: 0,
+        ),
+        barGroupingType: charts.BarGroupingType.grouped,
+        primaryMeasureAxis: new charts.NumericAxisSpec(renderSpec: new charts.NoneRenderSpec()),
+      ),
+    );
+  }
+}
 
 class PedometerReport extends StatelessWidget {
   const PedometerReport({super.key});
 
   @override
   Widget build(BuildContext context) {
+    List<BarChartData> chartData = [
+      BarChartData('S1', 6000),
+      BarChartData('M', 4000),
+      BarChartData('T1', 10408),
+      BarChartData('W', 2000),
+      BarChartData('T', 5366),
+      BarChartData('F', 9864),
+      BarChartData('S', 8524),
+    ];
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Pedometer Report'),
       ),
       body: Center(
-        child: Text('Pedometer Report'),
+        child: Container(
+          height: 300,
+          padding: EdgeInsets.all(0),
+          child: VerticalBarChart(chartData),
+        ),
       ),
     );
   }
--- lib/src/widgets/pedometer_bottom_navigation.dart
+++ lib/src/widgets/pedometer_bottom_navigation.dart
@@ -12,7 +12,7 @@ class PedoMeterBottomNavigation extends StatelessWidget {
       currentIndex: currentIndex,
       onTap: (value) => onTabChange(value),
       items: const [
-        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home', tooltip: 'Home'),
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Today', tooltip: 'Today'),
         BottomNavigationBarItem(icon: Icon(Icons.leaderboard_rounded), label: 'Report', tooltip: 'Report'),
         BottomNavigationBarItem(icon: Icon(Icons.health_and_safety_rounded), label: 'Health', tooltip: 'Health'),
         BottomNavigationBarItem(icon: Icon(Icons.settings_rounded), label: 'Settings', tooltip: 'Settings'),
